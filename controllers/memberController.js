35a23fc98b6ce547d9469a55f665657b4208cf0d
OdilovSarvarbek <odilovsarvar017@gmail.com>
2023-05-31 22:41:44 +0900

    feat: member login process development

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -9,18 +9,27 @@ memberController.signup = async (req, res) => {
         console.log("POST: cont/signup");
         const data = req.body;
         const member = new Member();
-        const new_member = await member.signupData(data );
+        const result = await member.signupData(data );
 
-        res.json({state: 'succeed', data: new_member});
+        res.json({state: 'succeed', data: result});
     }catch(err) {
         console.log("ERROR: cont/signup");
         res.json({state: "fail", message: err.message})
     }
 }
 
-memberController.login = (req, res) => {
-    console.log("POST--Login");
-    res.send("Login Pagedasiz")
+memberController.login = async (req, res) => {
+    try {
+        console.log("POST: cont/login");
+        const data = req.body;
+        const member = new Member();
+        const new_member = await member.loginData(data);
+
+        res.json({state: 'succeed', data: new_member});
+    }catch(err) {
+        console.log("ERROR: cont/login");
+        res.json({state: "fail", message: err.message})
+    }
 }
 
 memberController.logout = (req, res) => {
