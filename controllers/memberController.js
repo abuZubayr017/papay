a93ab3f1ca6fb6719fffdfe59aa79ef711d1ed91
OdilovSarvarbek <odilovsarvar017@gmail.com>
2023-07-15 00:36:45 +0900

    fix: checkMyAuthentication and retrieve token object data

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -12,11 +12,11 @@ memberController.signup = async (req, res) => {
       member = new Member(),
       new_member = await member.signupData(data);
 
-      const token = memberController.createToken(new_member);  
-      res.cookie("access_token", token, {
-        maxAge: 6 * 3600 * 1000,
-        httpOnly: true,
-      });
+    const token = memberController.createToken(new_member);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
 
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
@@ -63,6 +63,21 @@ memberController.createToken = (result) => {
 
     assert.ok(token, Definer.auth_err2);
     return token;
+  } catch (err) {
+    throw err;
+  }
+};
+
+memberController.checkMyAuthentication = (req, res) => {
+  try {
+    console.log("GET cont/checkMyAuthentication");
+    let token = req.cookies["access_token"];
+    console.log("token front: ", token);
+
+    const member = token ? jwt.verify(token, process.env.SECRET_TOKEN) : null;
+    assert.ok(member, Definer.auth_err2);
+
+    res.json({ state: "succeed", data: member });
   } catch (err) {
     throw err;
   }
